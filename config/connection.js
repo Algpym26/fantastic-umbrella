314d67e715eb75bce115d69b5b712b7e8933f350
Algpym26 <110873247+Algpym26@users.noreply.github.com>
2023-02-10 17:30:48 -0500

    dotenv

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,17 @@
-require('dotenv').config();
-
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+require('dotenv').config();
+
+const sequelize = new sequelize (
+  process.env.varName,
+  process.env.varUser,
+  process.env.varPassword,
+  {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+       
+  }
+);
 
 module.exports = sequelize;
